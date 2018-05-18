e57cdc2ab2af9fd1b66f7bcb646a429c11aedce5
Vadim Grigorov <chichavl@gmail.com>
2018-05-18 10:23:18 +0200

    Added docker-machine env

--- create_runner.sh
+++ create_runner.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+eval $(docker-machine env docker-host)
+
 docker run -d --name gitlab-runner --restart always \
 -v /srv/gitlab-runner/config:/etc/gitlab-runner \
 -v /var/run/docker.sock:/var/run/docker.sock \
